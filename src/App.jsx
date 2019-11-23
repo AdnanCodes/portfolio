3e60492a812eddc2210a589579f9760866e8631c
Adnan <adnan.chowdhury@outlook.com>
2019-11-22 21:06:09 -0600

    SASS conversion, removed premade boiler

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.scss";
+import Nav from "./components/Nav/Nav";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <header className="App-header">Lets make that website</header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Nav = () => {
+  return <>Add some nav bar</>;
+};
+
+export default Nav;
