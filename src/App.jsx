7de31270089c890456fcfcbda12ea4b4975ae969
Adnan <adnan.chowdhury@outlook.com>
2020-02-11 23:47:36 -0600

    packages added, google font api, bootstrap set

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSpring, animated, config } from "react-spring";
+import Button from "react-bootstrap/Button";
 import "./App.scss";
 import Nav from "./components/Nav/Nav";
 
@@ -12,6 +13,14 @@ function App() {
   return (
     <animated.div style={props}>
       <div className="App">
+        <h1>Hi! I am Adnan</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere
+          aliquid enim, dicta magnam ducimus harum rem accusamus maxime, eum ab
+          sed inventore doloribus reiciendis atque quos quia. Fugit, doloribus
+          assumenda.
+        </p>
+        <Button>Testing button</Button>
         <Nav />
         <header className="App-header">Let's make that website</header>
       </div>
