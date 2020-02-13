ca1b80b1ba6ffd45eb6c9b9aa548e6fbb20b0e15
Adnan <adnan.chowdhury@outlook.com>
2020-02-13 16:25:24 -0600

    basic home page layout done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 import Image from "react-bootstrap/Image";
-import hero from "../assets/4.jpg";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import hero from "../assets/mclaren.jpg";
+import profilePic from "../assets/me.jpg";
 const Home = () => {
-  return <Image src={hero} fluid />;
+  return (
+    <section>
+      <Image src={hero} fluid alt="McLaren P1 overshot in a race track" />
+      <Jumbotron className="intro">
+        <Image className="profile" src={profilePic} rounded />
+        <article>
+          <h1>Hey! I am Adnan</h1>
+          <p>
+            I am a full-stack web developer. I specialize in developing
+            front-end applications in ReactJS and building servers using Express
+            and Django.
+          </p>
+          <p>
+            My work experience has strengthened a customer-centric approach in
+            software development where I make it a priority to have a quality
+            user experience. In addition, I have acquired strong set of
+            problem-solving, organizational, and communication skills. I excel
+            in working in a team and a high-demand environment.
+          </p>
+          <p>
+            If I am not coding, I like to play video games, read books, cooking,
+            and think about improving education systems.
+          </p>
+        </article>
+      </Jumbotron>
+    </section>
+  );
 };
 
 export default Home;
