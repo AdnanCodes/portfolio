c3d34f3003f442ee8fe5de09325cc8bf0b9ce5c5
Adnan <adnan.chowdhury@outlook.com>
2020-03-02 23:23:45 -0600

    Blog Plan found

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+// https://hackernoon.com/building-a-blog-with-react-and-contentful-fd538f68f6fb
+// I think this would be good solution to implement and customize further with React Bootstrap
 const Blog = () => {
   return <section>Let's build Blog page</section>;
 };
