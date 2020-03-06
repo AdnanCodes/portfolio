da6ca070488ea72f4fd5fc3de96c3970eb362e89
Adnan <adnan.chowdhury@outlook.com>
2020-03-05 23:26:18 -0600

    More blog plans added

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -2,6 +2,10 @@ import React from "react";
 
 // https://hackernoon.com/building-a-blog-with-react-and-contentful-fd538f68f6fb
 // I think this would be good solution to implement and customize further with React Bootstrap
+// Perhaps design reusable blog components and prepare it part of npm package
+// Lot more research may be needed but components could be design in a reusable style with combinations of react bootstrap
+// Some of the challenges may expected. Integrated testing should be added as part
+// Blog should avoid any using routing but conditional rendering via accordion
 const Blog = () => {
   return <section>Let's build Blog page</section>;
 };
